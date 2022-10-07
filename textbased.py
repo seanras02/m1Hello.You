3bf5dfffa12c6116520b06a02cd9dcefcbc33828
Sean <34967@ma-web.nl>
2022-10-07 20:41:43 +0100

    super coole script hehe

---
+++ textbased.py
@@ -0,0 +1,106 @@
+print("Je kijkt naar het nieuws en ziet dat jou land op oorlog gaat met een andere land. (1940) ")
+print("Je gaat snel naar de evacuatie vliegtuigen")
+print("Welke land ga je naartoe voor veiligheid?")
+print("A. Je gaat naar het VK")
+print("B. Je gaat naar Frankrijk")
+print("C. Je gaat naar Duitsland ...")
+choice = input()
+if choice == "A":
+    print("Engels spreken is niet zo heel moeilijk maar, het accent is heel raar...")
+    input()
+    print("Je zit in het vliegtuig en het is de hele rit stil")
+    print("Je komt uiteindelijk aan in Engeland, maar ze willen dat je ook in de oorlog gaat vechten...")
+    print("A. Je besluit om te gaan helpen")
+    print("B. Je besluit om te onderduiken")
+
+    if choice == "A":
+        print("Je gaat meteen de oorlog in, maar je beseft heel laat dat je geen ervaring hebt")
+        print("dus je word opgepakt en gaat naar een concentratie kamp...")
+        input()
+        print("Je krijgt amper eten. Moet je aanjezelf denken of moet je meer aan anderen denken en helpen...?")
+        print("A. Je kiest voor jezelf en steelt eten van anderen om jezelf vol te maken")
+        print("B. Je besluit om te delen met anderen, maar je hebt heel veel honger...")
+
+        if choice == "A":
+            print("Je bent eigenwijs geworden, maar je overleeft de oorlog en komt erachter dat je het voor 5 jaar hebt overleven...")
+            print("Je voelt snel een golf van spijt, maar je hebt het overleeft...")
+            input()
+            print("Gefeliciteerd... Je leeft, maar met veel spijt...")
+
+        elif choice == "B":
+            print("Je bent heel erg bezig met de anderen te helpen, maar je merkt dat je heel zwak word...")
+            input()
+            print("Je overlijd, maar je word herdacht en word als held gezien !!!")
+
+
+    elif choice == "B":
+        print("Je gaat onderduiken zodat je niet moet vechten.")
+        input()
+        print("Je houd het voor 5 jaar vol, maar je word wel als vermist verklaard totdat je weer terug komt na de oorlog !")
+
+
+
+if choice == "B":
+    print("Frans is een hele rare taal...")
+    input()
+    print("Je zit nu in de vliegtuig en je hoort veel mensen praten over wat ze allemaal mee hebben gemaakt.")
+    print("Je wilt ook over je verhaal vertellen, dus je loopt naar hun toe en probeert om de mood meer op te vrolijken")
+    print("A. Je vertelt over de tijd toen je door eendjes aangevallen werd.")
+    print("B. Je vertelt over je leven voor de oorlog.")
+    choice = input()
+
+    if choice == "A":
+        print("Ze lachen je uit, maar ze vinden je verhaaltje leuk.")
+        input()
+        print("Je gaat verder met hun praten totdat je uiteindelijk bent geland")
+        print("Je komt erachter dat je toch heel veilig bent, dus je besluit om uiteindelijk werk te zoeken")
+        input()
+        print("Wat kies je?")
+        print("A. Je ziet een advertentie om voor de Engelsen te vechten in de oorlog")
+        print("B Je gaat in een fabriek te werken, maar je word niet veel betaald...")
+        choice = input()
+
+        if choice == "A":
+            print("Je gaat er naartoe, maar je werkt als EHBO")
+            print("Je doet het voor zo lang dat je er zo goed in bent, dat jet het oorlog overleeft dat je ook een medaille krijgt.")
+
+        elif choice == "B":
+            print("Je gaat in een fabriek werken, maar was het echt je komt elke dag moe met amper geld in zak...")
+            print("Maar je overleeft het wel en je krijgt uitendelijk een promotie waardoor je uiteindelijk een manager bent geworden!!")
+        
+    elif choice == "B": 
+        print("Ze vinden je een kut persoon omdat, je het situatie niet serieus neemt, maar bedoelt het niet zo...")
+        input()
+        print("Je loopt terug en besluit om het even te vergeten totdat je aankomt.")
+        print("Je hebt werk nodig, dus je je kijkt naar de opties.")
+        input("Wat kies je?")
+        print("A. Je ziet werk voor in de landmacht")
+        print("B. Je ziet werk voor in de luchtmacht")
+        choice = input()
+
+        if choice == "A":
+            print("Je gaat voor de landmacht vechten, maar je verstopt jezelf heel vaak...")
+            print("Je doet het voor zo lang dat je de oorlog hebt overleefd!!")
+
+
+        
+if choice == "C":
+    print("Duits klinkt alsof je iemand wilt vermoorden denk je bij jezelf...")
+    input()
+    print("Je bent nu in de viegtuig maar, het is heel stil...")
+    input()
+    print("Je denkt bij jezelf of je wel in de goede vliegtuig bent gestapt...")
+    print("Je komt erachter dat je in een nazi vliegtuig zit om voor de nazi's te gaan vechten. Wat ga je doen...?")
+    print("A. Je gaat bij de marine aansluiten")
+    print("B. Je gaat bij de landmacht aansluiten")
+    choice = input()
+
+    if choice == "A":
+        print("Je sluit aan bij de marine, maar je boot zonk uiteindelijk en je verdrinkt...")
+
+    elif choice == "B":
+        print("De nazi's verliezen dus je besluit om te onderduiken, maar je word uiteindelijk ")
+
+    
+
+
